aa37f562357588340239d61331c17542e70b5a9b
tangbojian <444325459@qq.com>
2017-05-20 13:49:27 +0800

    add edu.service

---
+++ src/main/java/com/tbj/edu/service/UserService.java
@@ -0,0 +1,14 @@
+package com.tbj.edu.service;
+
+import com.tbj.edu.entity.User;
+
+/**
+ * @author bjtang
+ * @date 2017Äê5ÔÂ20ÈÕ
+ * @desc userService
+ */
+public interface UserService {
+
+	public int add(User user);
+	
+}
